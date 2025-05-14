4b9a2ed3f0d07a778af25120e4353fa4cf1daa96
Marc Andrysco <marc.andrysco@gmail.com>
2025-05-14 11:46:39 -0500

    version 1.0.0-dev7

--- Makefile
+++
@@ -1,5 +0,0 @@
-all:
-	./hammer.cache .all pl3=$(PL3)
-
-check:
-	./hammer.cache .check pl3=$(PL3)
--- rt/posix/src/start.c
+++
@@ -1,9 +0,0 @@
-#include <errno.h>
-
-int geterr(void) {
-	return errno;
-};
-
-void seterr(int val) {
-	errno = val;
-}
